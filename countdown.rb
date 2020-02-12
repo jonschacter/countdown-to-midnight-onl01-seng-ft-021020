1d8e6834a6900148ee4cad2816133e21d0a6cfd5
Jon Schacter <schacter.jon@gmail.com>
2020-02-12 18:40:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,15 @@ def countdown(n)
   end
   "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(n)
+  time_remaining = n
+
+  while time_remaining > 0
+    puts "#{time_remaining} SECOND(S)!"
+    time_remaining -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
